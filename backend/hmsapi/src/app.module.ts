ce08afb82625154b105c03f91e6054ce8d9b92c9
Raj Kinkhabwala <kinkhabwalaraj@gmail.com>
2022-03-11 17:25:49 -0800

    Auth Services completed

--- backend/hmsapi/src/app.module.ts
+++ backend/hmsapi/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthConfig } from './config/auth.config';
 import { LocationController } from './controllers/location.controller';
 import { LocationService } from './services/location.service';
 import { PrismaService } from './services/prisma.service';
@@ -8,6 +9,6 @@ import { PrismaService } from './services/prisma.service';
 @Module({
   imports: [],
   controllers: [AppController, LocationController],
-  providers: [PrismaService, AppService, LocationService],
+  providers: [PrismaService, AppService, LocationService, AuthConfig],
 })
 export class AppModule {}
---
+++ backend/hmsapi/src/services/auth.service.ts
@@ -0,0 +1,121 @@
+import { AuthConfig } from '../config/auth.config';
+import { Inject, Injectable } from '@nestjs/common';
+import {
+  AuthenticationDetails,
+  CognitoUser,
+  CognitoUserPool,
+  CognitoUserAttribute,
+} from 'amazon-cognito-identity-js';
+
+@Injectable()
+export class AuthService {
+  private userPool: CognitoUserPool;
+  constructor(
+    @Inject('AuthConfig')
+    private readonly authConfig: AuthConfig,
+  ) {
+    this.userPool = new CognitoUserPool({
+      UserPoolId: this.authConfig.userPoolId,
+      ClientId: this.authConfig.clientId,
+    });
+  }
+
+  confirmRegistration(confirmUser: { code: string; email: string; }) {
+    const { code, email } = confirmUser;
+
+    const userData = {
+      Username: email,
+      Pool: this.userPool,
+    };
+
+    const cognitoUser = new CognitoUser(userData);
+
+    return new Promise((resolve, reject) => {
+      cognitoUser.confirmRegistration(code, true, function (err, result) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  registerUser(newUser: {
+    email: string;
+    password: string;
+    given_name: string;
+    family_name: string;
+    birthdate: string;
+    gender: string;
+  }) {
+    const { email, password, given_name, birthdate, gender, family_name } =
+      newUser;
+    return new Promise((resolve, reject) => {
+      this.userPool.signUp(
+        email,
+        password,
+        [
+          new CognitoUserAttribute({ Name: 'email', Value: email }),
+          new CognitoUserAttribute({ Name: 'given_name', Value: given_name }),
+          new CognitoUserAttribute({ Name: 'family_name', Value: family_name }),
+          new CognitoUserAttribute({ Name: 'birthdate', Value: birthdate }),
+          new CognitoUserAttribute({ Name: 'gender', Value: gender }),
+          new CognitoUserAttribute({
+            Name: 'updated_at',
+            Value: Date.now().toString(),
+          }),
+        ],
+        null,
+        (error, result) => {
+          if (!result) {
+            reject(error);
+          } else {
+            resolve(result);
+          }
+        },
+      );
+    });
+  }
+
+  authenticateUser(user: { email: string; password: string }) {
+    const { email, password } = user;
+
+    const authenticationDetails = new AuthenticationDetails({
+      Username: email,
+      Password: password,
+    });
+    const userData = {
+      Username: email,
+      Pool: this.userPool,
+    };
+
+    const newUser = new CognitoUser(userData);
+
+    return new Promise((resolve, reject) => {
+      newUser.authenticateUser(authenticationDetails, {
+        onSuccess: (result) => {
+          resolve(result);
+        },
+        onFailure: (err) => {
+          console.log(err);
+          reject(err);
+        },
+        newPasswordRequired: (userAttributes, requiredAttributes) => {
+          newUser.completeNewPasswordChallenge(
+            'Admin@1234@raj',
+            requiredAttributes,
+            {
+              onSuccess: (result) => {
+                resolve(result);
+              },
+              onFailure: (err) => {
+                reject(err);
+              },
+            },
+          );
+        },
+      });
+    });
+  }
+}
