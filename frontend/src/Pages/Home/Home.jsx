73e9119f57931ab1a5763f2c0b5a529440320053
Varun Alla <varun.alla@sjsu.edu>
2022-03-16 15:11:58 -0700

    fixed routing issue

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -2,34 +2,20 @@ import React from "react";
 import TopBar from "../../components/TopBar";
 import { Center } from "@mantine/core";
 import { Link } from "react-router-dom";
+import SearchComponent  from "../../components/SearchComponent";
 export function Home() {
   const links = [
-    {
-      link: "/about",
-      label: "Features",
-    },
-    {
-      link: "/pricing",
-      label: "Pricing",
-    },
-    {
-      link: "/learn",
-      label: "Learn",
-    },
-    {
-      link: "/community",
-      label: "Community",
-    },
+  
   ];
 
   return (
     <>
       <TopBar links={links} />
-      {/* Search Bar for location and initial data will go here. */}
-      <Center>
       <SearchComponent/>
-        <div>All elements inside Center are centered</div>
-        <Link to="/listing">Listing</Link>
+      <Center>
+      
+      { /* <Link to="/listing">Listing</Link>Search Bar for location and initial data will go here. */}
+        
       </Center>
     </>
   );
