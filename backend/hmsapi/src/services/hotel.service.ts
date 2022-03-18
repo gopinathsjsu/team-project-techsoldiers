2be4f0fe538450dec8c09111ff8020cbad8c8c11
Sakshi Jain <sakshi.jain01@sjsu.edu>
2022-03-18 16:45:13 -0700

    DB connection added

--- backend/hmsapi/src/services/hotel.service.ts
+++ backend/hmsapi/src/services/hotel.service.ts
@@ -6,7 +6,7 @@ import { PrismaService } from './prisma.service';
 export class HotelService {
   constructor(private prisma: PrismaService) {}
   async hotels(): Promise<Hotel[] | null> {
-    return [{"id":1,"name":"Marriot","locationid":1,"description":"Previous Hotel"},{"id":2,"name":"Hyatt","locationid":1,"description":"New Hotel added"},{"id":3,"name":"PristinePro","locationid":2,"description":"New location hotel"}];
+    return this.prisma.hotel.findMany({});
   }
 
 
