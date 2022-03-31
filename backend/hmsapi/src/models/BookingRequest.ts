1a6dfd1cb9b5592447106782163b18ae4e818e18
Sakshi Jain <sakshi.jain01@sjsu.edu>
2022-03-31 14:11:15 -0700

    Change in schema for hotel, added bookings field

---
+++ backend/hmsapi/src/models/BookingRequest.ts
@@ -0,0 +1,9 @@
+export interface BookingRequest { 
+    bookingToDate: Date;
+     bookingFromDate : Date; 
+     customerId : Number; 
+     totalPrice: number ; 
+     status : string;
+     hotelId: Number 
+    }
+  
