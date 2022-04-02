f426853e803d32669764bfd3358f7ea9e0c966d5
Sakshi Jain <sakshi.jain01@sjsu.edu>
2022-04-02 15:19:30 -0700

    added service to create booking

--- backend/hmsapi/src/controllers/booking.controller.ts
+++ backend/hmsapi/src/controllers/booking.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param , Body,Post} from '@nestjs/common';
 import { BookingService } from 'src/services/booking.service';
 import { Booking as BookingModel } from '.prisma/client';
+import { BookingRequest } from 'src/models/BookingRequest';
 @Controller('api/booking')
 export class BookingController {
     constructor(private readonly bookingService: BookingService) { }
@@ -23,5 +24,25 @@ export class BookingController {
                 hotelId: Number(id),
             },
         });
+    }
+
+    @Post()
+    async createBooking(
+        @Body() bookingData: BookingRequest,
+    ): Promise<BookingModel> {
+        const { bookingToDate, bookingFromDate, customerId, totalPrice, hotelId, status } = bookingData;
+        return await this.bookingService.createBooking({
+            bookingToDate,
+            bookingFromDate,
+            totalPrice,
+            status: status,
+            hotel: {
+                connect: { id: Number(hotelId) },
+            },
+            customer: {
+                connect: { customerId: Number(customerId) },
+            }
+        });
+
     }
 }
