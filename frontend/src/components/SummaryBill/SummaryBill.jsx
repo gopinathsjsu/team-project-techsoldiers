dda3a047ab32ae582be73ab34c1e1a620bbc58cc
Varun Alla <varun.alla@sjsu.edu>
2022-05-10 15:44:17 -0700

    added billing summary

--- frontend/src/components/SummaryBill/SummaryBill.jsx
+++ frontend/src/components/SummaryBill/SummaryBill.jsx
@@ -5,18 +5,23 @@ import { useState } from 'react';
 
 const useStyles = createStyles((theme) => ({
     card: {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
     },
-  
+
     title: {
-      fontWeight: 700,
-      fontSize: 20,
-      fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-      lineHeight: 1.2,
+        fontWeight: 700,
+        fontSize: 20,
+        fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+        lineHeight: 1.2,
     },
-  
+    content: {
+        fontWeight: 400,
+        fontSize: 12,
+        fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+        lineHeight: 1,
+    },
     body: {
-      padding: theme.spacing.md,
+        padding: theme.spacing.md,
     },
 
     border: {
@@ -24,33 +29,43 @@ const useStyles = createStyles((theme) => ({
     },
 }));
 
-const RoomRow=(props)=>{
+const RoomRow = (props) => {
     return <>
-    <div>{props.roomType}</div>
+        <div>{props.roomType}</div>
     </>
 }
 
 
 export const SummaryBill = (props) => {
     const { classes } = useStyles();
-    let {  hotelName, person, room, roomType } = props.bookings;
-    const { data: roomPrice , error, isError, isLoading } = useQuery(['getRoomPrice', props.bookings.roomID],() => getRoomPriceFinal(props.bookings.hotelID, props.bookings.roomID, props.bookings.date.from, props.bookings.date.to), {
+    let { hotelName, person, room, roomType } = props.bookings;
+    console.log('rooms props ', props.rooms);
+    const { data: roomPrice, error, isError, isLoading } = useQuery(['getRoomPrice', props.bookings.roomID], () => getRoomPriceFinal(props.bookings.hotelID, props.bookings.roomID, props.bookings.date.from, props.bookings.date.to), {
         suspense: true
     });
 
-    if(isLoading){
+    if (isLoading) {
         <Text>Loading...</Text>
     }
-    
+    if(roomPrice){
+        console.log(roomPrice)
+    }
+
     return (
-    <>
-        <Box className={classes.border}>
-            <Text className={classes.title} mt={20} mb={20} transform="uppercase">Total Bill</Text>
-            <Text className={classes.title} mt={20} mb={20}>Total Base Price: {(parseInt(roomPrice.data.basePrice) * 7)* room}</Text>
-            {/* {props.rooms&&props.rooms.map((room)=>{
-                return <RoomRow {...room}/>
+        <>
+            <Box className={classes.border}>
+                <Text className={classes.title} mt={20} mb={20} transform="uppercase">Total Bill</Text>
+                <Text className={classes.content} mt={20} mb={20}>Base Price Per Room for given day: {(parseInt(roomPrice.data.basePrice) )}</Text>
+                <Text className={classes.content} mt={20} mb={20}>Total Dynamic Price Per Room for all days: {(parseInt(roomPrice.data.finalSurgePrice) )}</Text>
+                <Text className={classes.content} mt={20} mb={20}>Total Dynamic Price: {(parseInt(roomPrice.data.finalSurgePrice) ) * room}</Text>
+                <Text className={classes.content} mt={20} mb={20}>Amenities Price: {props.amprice}</Text>
+                <Text className={classes.title} mt={20} mb={20}>Final Price: {((parseInt(roomPrice.data.finalSurgePrice) ) * room)+props.amprice}</Text>
+              
+                {/* {props.rooms&&props.rooms.map((room)=>{
+         
+         return <RoomRow {...room}/>
             })} */}
-        </Box>
-    </>
+            </Box>
+        </>
     )
 }
--- frontend/src/pages/BookingSummary/BookingSummary.jsx
+++ frontend/src/pages/BookingSummary/BookingSummary.jsx
@@ -23,53 +23,66 @@ export function BookingSummary() {
       if (response.data.length > 0) {
         console.log(' if response = ', response.data)
 
-        setAmenityData(response.data.map((e) => {return {...e, id: '' + e.id}}));
-}
+        setAmenityData(response.data.map((e) => { return { ...e, id: '' + e.id } }));
+      }
     }
-fetchData();
+    fetchData();
   }, []);
   const auth = useSelector(state => state.login);
 
-let bookingRooms = [];
-//check if rooms are already configured in state
-//if yes retrieve them
-//else create
-for (let i = 0; i < bookings.room; i++) {
-  bookingRooms.push({
-    roomIndex: i,
-    roomType: bookings.roomType,
-    selectedAmenities: [],
-  });
-}
-console.log(bookingRooms);
-function changeRoomAmenities(roomid, amenities) {
-  console.log(roomid + " ", amenities);
-  bookingRooms[roomid].selectedAmenities=amenities;
-}
-function bookNow(){
+  let bookingRooms = [];
+  //check if rooms are already configured in state
+  //if yes retrieve them
+  //else create
+  for (let i = 0; i < bookings.room; i++) {
+    bookingRooms.push({
+      roomIndex: i,
+      roomType: bookings.roomType,
+      selectedAmenities: [],
+    });
+  }
   console.log(bookingRooms);
-}
+  let [amprice, setAmprice] = useState(0);
+  function changeRoomAmenities(roomid, amenities) {
+    console.log(roomid + " ", amenities);
+    bookingRooms[roomid].selectedAmenities = amenities;
+    calculateAmenities(bookingRooms);
+  }
+  function calculateAmenities(bookingRooms) {
+    for (let bookingRoom of bookingRooms) {
+      let amen = amenityData.filter(e => bookingRoom.selectedAmenities.indexOf(e.id));
+      let prices = 0;
+      amen.forEach(e => {
+        prices += parseInt(e.price);
+      });
+      setAmprice(amprice + prices);
+    }
+    console.log(amprice);
+  }
+  function bookNow() {
+    console.log(bookingRooms);
+  }
 
-/*
- 
-*/
-return (
-  <>
-    <Container>
-      <BookingDetails bookings={bookings} />
-      <Grid mb={20}>
-      {bookingRooms.length > 0 && bookingRooms.map((room) => {
-          return <Grid.Col span={6}><BookingDetailsRoom key={room.roomIndex} amenities={amenityData} details={room} changeRoomAmenities={changeRoomAmenities} /></Grid.Col>
-      })}
-      </Grid>
-      {/* {bookingRooms.length > 0 && bookingRooms.map((room) => {
+  /*
+   
+  */
+  return (
+    <>
+      <Container>
+        <BookingDetails bookings={bookings} />
+        <Grid mb={20}>
+          {bookingRooms.length > 0 && bookingRooms.map((room) => {
+            return <Grid.Col span={6}><BookingDetailsRoom key={room.roomIndex} amenities={amenityData} details={room} changeRoomAmenities={changeRoomAmenities} /></Grid.Col>
+          })}
+        </Grid>
+        {/* {bookingRooms.length > 0 && bookingRooms.map((room) => {
 
         return <BookingDetailsRoom key={room.roomIndex} amenities={amenityData} details={room} changeRoomAmenities={changeRoomAmenities} />
       })} */}
-      <SummaryBill  bookings={bookings} rooms={bookingRooms} />
-      <Button mt={30} onClick={bookNow}>Confirm Booking</Button>
-    </Container>
-  </>
+        <SummaryBill bookings={bookings} rooms={bookingRooms} amprice={amprice} />
+        <Button mt={30} onClick={bookNow}>Confirm Booking</Button>
+      </Container>
+    </>
 
-);
+  );
 }
